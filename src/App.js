00319f8229528e78d5e8e21ddd8796e19af177ff
Marina Weis <marinaweisproductions@gmail.com>
2020-12-14 19:45:17 -0500

    initial commit, needs bug fixed & class refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor(props) {
@@ -29,11 +30,12 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>
-            Character sheet - let's get started!
-          </h1>
+         
 
           <div class='enterTrait'>
+          <p>
+            Character sheet - let's get started!
+          </p>
             <p>Strength:</p>
             <input
               value={this.state.strength}
@@ -62,5 +64,50 @@ class App extends React.Component {
     );
   }
 }
+
+
+
+//THIS CODE BELOW IS FROM THE INTERNET. IT MAKES FUN ANIMATED BACKGROUND HAPPEN
+
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+
+const numBalls = 50;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
 
 export default App;
