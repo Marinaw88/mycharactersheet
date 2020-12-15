3846f16f07ce00c968a3f0d69bd70f5357e2cdef
Marina Weis <marinaweisproductions@gmail.com>
2020-12-14 20:39:24 -0500

    Major refactoring, added variables & new class, for loop

--- README.md
+++ README.md
@@ -2,12 +2,26 @@
 
  Changing Input States in React -- the Dungeons and Dragons way
 
+
+Update!
+1. refactored by adding another class and scrapping the hard coded abilities for state variables
+2. Changed class names for clarity
+3. Added a for loop to remove repition 
+
+
  Goals:
- 1. Learn how React heiarchy works
+ 1. Learn how React heiarchy works <done>
  2. Import external styling <done>
 
 To do:
-1. Fix input bug - make functional
-2. Need to refactor and create more classes so it's not repeating and it's not hardcoded. 
+1. Fix input bug - make functional <done>
+2. Need to refactor and create more classes so it's not repeating and it's not hardcoded. <done>
+
+
+
+
+
+
+
 
 
--- src/App.js
+++ src/App.js
@@ -2,68 +2,103 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+class Abilities extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      strength: 0,
-      brains: 0,
-      stamina: 0,
-      dexterity: 0
+      justNum: "",
+      calcNum: ""
     };
   }
+calculating = (myEvent) => {
+  let value = myEvent.target.value;
+  let calculatedValue = (value-10)/2;
+  this.setState({
+    justNum: value,
+    calcNum: calculatedValue 
+  })
+}
+render (){
+  return(
+    <div>
+      <input
+      value={this.state.justnum}
+      onChange={this.calculating}
+      />
+    <h3>{this.props.name} Calculated: {this.state.calcNum}</h3>
+    </div>
+  );
+}
+}
 
-  changeStrength = (myEvent) => {
-    this.setState({
-      strength: myEvent.target.value
-    });
-  }
-
-  changeBrains = (myEvent) => {
-    this.setState({
-      brains: myEvent.target.value
-    });
-  }
-
+class App extends React.Component {
   render() {
+    let abilities = ["Strength", "Brains"];
+    let displayInfo = []
+    for (const num in abilities){
+      displayInfo.push(<Abilities name={abilities[num]}/>)
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-         
-
-          <div class='enterTrait'>
-          <p>
-            Character sheet - let's get started!
-          </p>
-            <p>Strength:</p>
-            <input
-              value={this.state.strength}
-              onChange={this.stateStrength}
-            />
-            <p>Brains:</p>
-            <input
-              value={this.state.brains}
-              onChange={this.changeBrains}
-            />
-          </div>
-          <div class='displayInfo'>
-            <div class='strengthInfo'>
-              <p>Strength Information</p>
-              <p class='dispStrength'> {this.state.strength}</p>
-              <p class='strengthMod'>{(this.state.strength - 10 / 2)}</p>
-            </div>
-            <div class='brainsInfo'>
-              <p>Brains Information</p>
-              <p class='dispBrains'>{this.state.brains}</p>
-              <p class='brainsMod'>{(this.state.brains - 10) / 2}</p>
-            </div>
-          </div>
-        </header>
+      <div>
+        <h1>Let's Calculate Your Abilities!</h1>
+        <h2>Enter Your Points to Convert Them</h2>
+        {displayInfo}
       </div>
     );
   }
 }
+
+//   changeStrength = (myEvent) => {
+//     this.setState({
+//       strength: myEvent.target.value
+//     });
+//   }
+
+//   changeBrains = (myEvent) => {
+//     this.setState({
+//       brains: myEvent.target.value
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+         
+
+//           <div class='enterTrait'>
+//           <p>
+//             Character sheet - let's get started!
+//           </p>
+//             <p>Strength:</p>
+//             <input
+//               value={this.state.strength}
+//               onChange={this.stateStrength}
+//             />
+//             <p>Brains:</p>
+//             <input
+//               value={this.state.brains}
+//               onChange={this.changeBrains}
+//             />
+//           </div>
+//           <div class='displayInfo'>
+//             <div class='strengthInfo'>
+//               <p>Strength Information</p>
+//               <p class='dispStrength'> {this.state.strength}</p>
+//               <p class='strengthMod'>{(this.state.strength - 10 / 2)}</p>
+//             </div>
+//             <div class='brainsInfo'>
+//               <p>Brains Information</p>
+//               <p class='dispBrains'>{this.state.brains}</p>
+//               <p class='brainsMod'>{(this.state.brains - 10) / 2}</p>
+//             </div>
+//           </div>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 
 
